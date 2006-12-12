0af48a3f5edc5b1e3f6f28ce03bd1ddd73197694
Henrik Tidefelt <tidefelt@isy.liu.se>
2006-12-12 23:19:28 +0000

    Automated testing has been introduced in the project.  The tests are divided into two cathegories; text and graphics.  To setup a new test, one adds the drool source to the appropriate directory, makes the correct output (by using make, see the Makefile in the directory in question), and adds both files to the repository.
    To run all the tests, one changes to the ./test directory, and does
      make clean
      make
      cat report
    
    I have included one test in each cathegory that fails miserably, with the intent of testing the test procedure itself.  These tests should be labeled with failure, but not interupt the automated test process.
    
    
    
    git-svn-id: https://svn.isy.liu.se/rt/tidefelt/source/drool/trunk@316 c1f300ed-6a01-0410-bd5d-8e39f192a3d9

--- source/metapdfparser.tab.h
+++ source/metapdfparser.tab.h
@@ -1,7 +1,9 @@
-/* A Bison parser, made by GNU Bison 1.875c.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
-/* Skeleton parser for Yacc-like parsing with Bison,
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -15,13 +17,21 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
-/* As a special exception, when this file is copied by Bison into a
-   Bison output file, you may use that output file without restriction.
-   This special exception was added by the Free Software Foundation
-   in version 1.24 of Bison.  */
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
 
 /* Tokens.  */
 #ifndef YYTOKENTYPE
@@ -82,6 +92,7 @@
      T_dynamiccolon = 308
    };
 #endif
+/* Tokens.  */
 #define T_EOF 258
 #define T_minusminus 259
 #define T_plusplus 260
@@ -137,9 +148,10 @@
 
 
 
-#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
 #line 92 "metapdfparser.y"
-typedef union YYSTYPE {
+{
   Ast::Expression * expr;
   Ast::Node * node;
   Ast::MethodIdExpr * methodId;
@@ -163,9 +175,10 @@ typedef union YYSTYPE {
   Ast::MemberMode memberMode;
   Ast::ClassMode classMode;
   Ast::FunctionMode functionMode;
-} YYSTYPE;
-/* Line 1275 of yacc.c.  */
-#line 169 "metapdfparser.tab.h"
+}
+/* Line 1529 of yacc.c.  */
+#line 181 "metapdfparser.tab.h"
+	YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 # define YYSTYPE_IS_TRIVIAL 1
@@ -173,7 +186,7 @@ typedef union YYSTYPE {
 
 extern YYSTYPE metapdflval;
 
-#if ! defined (YYLTYPE) && ! defined (YYLTYPE_IS_DECLARED)
+#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
 typedef struct YYLTYPE
 {
   int first_line;
@@ -187,5 +200,3 @@ typedef struct YYLTYPE
 #endif
 
 extern YYLTYPE metapdflloc;
-
-
